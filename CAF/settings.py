169eaa80ab300e4d977bec27ec40427189669b4a
Rharuow <haryssonsoares@gmail.com>
2020-04-07 14:34:49 -0300

    adjustments

--- CAF/settings.py
+++ CAF/settings.py
@@ -30,8 +30,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'widget_tweaks',
     'django_extensions',
+    'sass_processor',
 
     'home',
+    'client',
+    'session',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,21 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     STATIC_DIR,
 )
+
+SASS_PROCESSOR_INCLUDE_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
+
+SASS_PROCESSOR_ENABLED = True
--- CAF/urls.py
+++ CAF/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('client/', include('client.urls')),
+    path('session/', include('session.urls')),
     path('admin/', admin.site.urls),
 ]
