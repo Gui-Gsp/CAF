5b74bcb42a597c0f70e907e650eb9075e55d6d09
Rharuow <haryssonsoares@gmail.com>
2020-04-22 06:11:55 -0300

    events animation aside-bar

--- static/js/client/animations/icon-aside-bar.js
+++ static/js/client/animations/icon-aside-bar.js
@@ -1,3 +1,9 @@
 function toogleIcon(element) {
-    console.log(element)
+    if( !element.className.includes("activate") ) {
+        element.childNodes[5].className = "fas fa-chevron-down"
+        element.className = "topic pointer activate"
+    } else {
+        element.childNodes[5].className = "fas fa-chevron-right"
+        element.className = "topic pointer"
+    }
 }
